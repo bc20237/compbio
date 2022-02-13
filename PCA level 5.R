f66e64b047d3884d591eb431d1c3a2c6469d703f
bc20237 <bc120237@gmail.com>
2022-02-13 12:17:19 -0500

    pca

--- PCA level 5.R
+++ PCA level 5.R
@@ -13,16 +13,18 @@ library(ggbiplot)
 tl5<-taxlevel5
 require(plyr)
 
-print(names(tl5[,c(2:27)]))
-tl5.pca<-prcomp(tl5[,c(2:27)], center=TRUE, scale.=TRUE)
+print(names(tl5[,c(2:38)]))
+tl5.pca<-prcomp(tl5[,c(2:38)], center=TRUE, scale.=TRUE)
 #look at the table output
 summary(tl5.pca)
 str(tl5.pca)
 colrs <- adjustcolor( c("green","blue","red"), alpha=.6)
-pcaPlot<-(ggbiplot(tl5.pca, obs.scale = 1, var.scale = 1, 
+
+pcaPlot<-(ggbiplot(tl5.pca, obs.scale = 2, var.scale = 2, 
          groups=tl5[,321],
    ellipse = TRUE) + 
+     
   scale_color_discrete(name = '') +
-  theme(legend.direction = 'horizontal', legend.position = 'top'))
+  theme(legend.direction = 'horizontal', legend.position = 'top')) + ggtitle('Principal Component Analysis, \n family level') + theme(plot.title = element_text(size=20))+ theme(axis.title.x = element_text(size=20)) + theme(axis.title.y = element_text(size=20))
 pcaPlot
 
